7f697381278f76eb36d179225b248e0d37331ec8
Vinh Ngu <vinh-ngu@hotmail.com>
2019-11-12 01:12:45 +0100

    Use env 'GITLAB_SERVER'.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 GITLAB_GROUP_ID = os.getenv("GITLAB_GROUP_ID")
 GITLAB_PRIVATE_TOKEN = os.getenv("GITLAB_PRIVATE_TOKEN")
-GITLAB_API_BASE_URL = os.getenv("GITLAB_API_BASE_URL")
+GITLAB_API_BASE_URL = "{}/api/v4".os.getenv("GITLAB_SERVER")
 
 TAGS_TO_KEEP = os.getenv('TAGS_TO_KEEP', "latest").split(',')
 DELETE_TAGS_WITH_PREFIX = os.getenv("DELETE_TAGS_WITH_PREFIX", "").split(',')
